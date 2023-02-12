431e279a73a4b287083c25eb5e371421bb695ba6
Sophie Hirn <sophie.hirn@wyvernscale.com>
2023-02-12 12:31:34 +0100

    fuck

---
+++ base.lua
@@ -0,0 +1,147 @@
+local base = {}
+
+local util = require "hmm.util"
+
+--  Extracting Archives  ===============================================================================================
+
+function base.unpack(archive, target)
+	local _, ext = util.extsplit(archive)
+	local f = base.archivemap[ext]
+	if not f then util.error("cannot open arcive of type %s", ext) end
+	f(archive, target)
+end
+
+base.archivemap = {}
+
+base.archivemap["7z"] = function(archive, target) assert(util.exec("7z x -y -o%s %s >/dev/null", target, archive)) end
+base.archivemap.rar   = function(archive, target) assert(util.exec("unrar x -y %s %s >/dev/null", archive, target)) end
+base.archivemap.zip   = base.archivemap["7z"]
+
+--  Mod Base Implementation  ===========================================================================================
+
+base.modmt = {__index = {}}
+
+--  Must Be Implemented  -----------------------------------------------------------------------------------------------
+
+-- Resolve internal information after object has been created, runs before all
+-- other functions.
+--
+-- Required fields to be filled in by this function:
+--
+--  - path (string): base path below `cachedir .. "/mods"` for all files
+--  - id (string): unique identifier for the mod within this deployment
+--  - name (string): human-readable name for the mod
+--  - url (string): mod page url
+function base.modmt.__index:resolve()
+end
+
+-- Get files. The result is a list of objects with the following fields:
+--
+--  - filename (string): sensible file name without any path components
+--  - url (string | function): download url, possibly wrapped in a function,
+--      which is only to be called if the download is actually carried out
+--  - id (string): identifier of some kind, unique within this mod
+function base.modmt.__index:getfiles()
+	return {}
+end
+
+--  May Be Implemented  ------------------------------------------------------------------------------------------------
+
+-- Returns a map of keys that are okay for the user to set. The values in this
+-- table can either be `true` or a function, through which the user-supplied
+-- value is passed before setting.
+function base.modmt.__index:userkeys()
+	return {
+		redownload = true,
+		reunpack = true,
+		reinstall = true,
+		collisions = util.toset,
+	}
+end
+
+-- Get dependencies as mod objects.
+function base.modmt.__index:getdeps()
+	return {}
+end
+
+-- Default implementation for the download phase. Only redownloads if prompted.
+function base.modmt.__index:download()
+	assert(util.exec('mkdir -p %s', self:downloadpath()))
+	for _, f in ipairs(self:getfiles()) do
+		local path = self:downloadpath(f.filename)
+		if self.redownload or not util.exec("find %s -type f >/dev/null 2>&1", path) then
+			util.log("Download %s", f.filename)
+			assert(util.exec("wget --quiet --output-document=%s %s", path, type(f.url) == "function" and f.url() or f.url))
+		end
+	end
+end
+
+-- Default implementation for the unpack phase. Only reunpacks if prompted.
+function base.modmt.__index:unpack()
+	for _, f in ipairs(self:getfiles()) do
+		local src = self:downloadpath(f.filename)
+		local dst = self:unpackpath(f.filename)
+		if self.reunpack or not util.exec("find %s -type d >/dev/null 2>&1", dst) then
+			util.exec('rm -r %s 2>/dev/null', dst)
+			assert(util.exec('mkdir -p %s', dst))
+			util.log("Unpack %s", f.filename)
+			base.unpack(src, dir)
+		end
+	end
+end
+
+-- Default implementation for the prepare phase, does nothing.
+function base.modmt.__index:prepare()
+end
+
+-- Default implementation for the install phase, copies files from all the
+-- unpacked directories into the root of the install directory.
+function base.modmt.__index:install()
+	local dst = self:installpath()
+	if self.reinstall or not util.exec("find %s -type d >/dev/null 2>&1", dst) then
+		util.exec("rm -r %s 2>/dev/null", dst)
+		assert(util.exec("mkdir -p %s", dst))
+		for _, f in ipairs(self:getfiles()) do
+			util.log("Install %s", f.filename)
+			assert(util.exec("rsync --quiet --archive %s/ %s", self:unpackpath(f.filename), dst))
+		end
+	end
+end
+
+--  Interface Functions  -----------------------------------------------------------------------------------------------
+
+function base.modmt:__call(data)
+	local map = self:userkeys()
+	for k, v in pairs(data) do
+		    if type(map[k]) == "function" then self[k] = map[k](v)
+		elseif      map[k]                then self[k] =        v
+		else self:error("unknown property: %s = %s", tostring(k), tostring(v))
+		end
+	end
+end
+
+function base.modmt.__index:downloadpath(filename)
+	return self.path .. "/download" .. (filename and ("/" .. filename) or "")
+end
+
+function base.modmt.__index:unpackpath(filename)
+	if filename then
+		return self.path .. "/unpack/" .. (util.extsplit(filename))
+	else
+		return self.path .. "/unpack"
+	end
+end
+
+function base.modmt.__index:installpath()
+	local base = "with"
+	for _, f in ipairs(self:getfiles()) do base = base .. "-" .. f.id end
+	return self.path .. "/install/" .. base
+end
+
+--  Utility Functions    -----------------------------------------------------------------------------------------------
+
+function base.modmt.__index:error(...)
+	util.error("\nWhile processing %q (%s)\nAn error occured: %s", self.name, self.url, fmt:format(...))
+end
+
+return base
---
+++ modules/nexusmods.lua
@@ -0,0 +1,183 @@
+local nexus = {}
+
+local util = require "hmm.util"
+local json = require "hmm.json"
+local base = require "hmm.base"
+
+--  API  ===============================================================================================================
+
+local domain = "nexusmods.com"
+local apidomain = "api." .. domain
+local webdomain = "www." .. domain
+
+nexus.apikey = false
+
+local function api(path, validity, fmt, ...)
+	if not util.exec("find %s -type f -mmin -%s >/dev/null 2>&1", path, validity) then
+		if not nexus.apikey then util.error("nexus.apikey required") end
+		local url = ("https://%s/" .. fmt):format(apidomain, ...)
+		util.log("Query %s", url)
+		local cmd = table.concat({"curl",
+			"--silent",
+			"--request", "GET",
+			"--header", "'accept: application/json'",
+			"--header", ("'apikey: %s'"):format(nexus.apikey),
+			"--output", util.shellesc(path),
+			"--create-dirs",
+			util.shellesc(url)
+		}, " ")
+		assert(os.execute(cmd))
+	end
+	return json.decode(assert(io.open(path, "r")):read("a"))
+end
+
+--  Games  =============================================================================================================
+
+local game = false
+
+function nexus.game(name)
+	local self = {}
+
+	self.name = name
+	self._info = api(("%s/api/nexus/%s/info.json"):format(cachedir, name), 30 * 24 * 60, "v1/games/%s.json", name)
+
+	self.id = self._info.id
+	self.displayname = self._info.name
+
+	game = self
+end
+
+--  Mod Class  =========================================================================================================
+
+nexus.modmt = {__index = {}}
+setmetatable(nexus.modmt.__index, base.modmt)
+nexus.modmt.__call = base.modmt.__call
+
+local cache = {}
+
+function nexus.mod(url)
+	if not cache[url] then
+		local self = {}
+		self.game, self.id = url:match("^https://www.nexusmods.com/([^/]*)/mods/([0-9]*)$")
+		if not self.game then util.error("could not parse URL: %s", url) end
+		if not game then util.error("no game specified before defining mods") end
+
+		self.url  = url
+		self.name = "?"
+
+		self.path = cachedir .. "/mods/nexus/" .. self.id
+		self.apipath = self.path .. "/api"
+		setmetatable(self, nexus.modmt)
+
+		if self.game ~= game.name then self:error("game set to %s, but this mod is for %s", game.name, self.game) end
+
+		cache[url] = self
+	end
+
+	return cache[url]
+end
+
+function nexus.modmt.__index:userkeys()
+	local ret = base.modmt.__index.userkeys(self)
+	ret.deps = true
+	ret.files = true
+	ret.ignoredeps = util.toset
+	return ret
+end
+
+function nexus.modmt.__index:resolve()
+	if not self._resolved then
+		self._info  = api(self.apipath .. "/info.json" , 23 * 60, "v1/games/%s/mods/%s.json"      , self.game, self.id)
+		self._files = api(self.apipath .. "/files.json", 23 * 60, "v1/games/%s/mods/%s/files.json", self.game, self.id)
+
+		self.name = self._info.name
+
+		if not self.files then
+			local primary = false
+			for _, v in ipairs(self._files.files) do
+				if v.category_id == 1 then
+					if primary then
+						util.errmsg("%q (%s):\nmod consists of multiple files, please specify their IDs manually", self.name, self.url)
+						util.log("\nfound files:")
+						for _, f in ipairs(self._files.files) do
+							if f.category_id ~= 4 and f.category_id ~= 7 then
+								util.log("%s %d: %s", f.category_name, f.file_id, f.name)
+							end
+						end
+						util.log("\nMore info on %s?tab=files\n", self.url)
+						os.exit(1)
+					end
+					primary = v.file_id
+				end
+			end
+			if not primary then self:error("no primary file found, please specify file IDs manually") end
+
+			self.files = {primary}
+		end
+
+		self._resolved = true
+	end
+end
+
+function nexus.modmt.__index:getfiles()
+	local ret = {}
+	for _, fileid in ipairs(self.files) do
+		local f = false
+		for _, v in ipairs(self._files.files) do
+			if v.file_id == fileid then
+				local _, ext = util.extsplit(v.file_name)
+				f = {}
+				f.id       = ("%d"):format(fileid)
+				f.filename = f.id .. "." .. ext
+				f.url      = function()
+					return api(self.apipath .. "/download/" .. f.id .. ".json", 1,
+						"v1/games/%s/mods/%s/files/%s/download_link.json",
+						self.game,
+						self.id,
+						f.id
+					)[1].URI
+				end
+				break
+			end
+		end
+		if not f then self:error("could not find file %s", fileid) end
+		table.insert(ret, f)
+	end
+	return ret
+end
+
+function nexus.modmt.__index:getdeps()
+	assert(util.exec("mkdir -p %s/deps", self.path))
+
+	local path = self.path .. "/deps/scraped.html"
+	local url = ("https://%s/Core/Libs/Common/Widgets/ModDescriptionTab?id=%d&game_id=%d"):format(webdomain, self.id, game.id)
+
+	if not util.exec("find %s -type f -mmin -%s >/dev/null 2>&1", path, 23 * 60) then
+		assert(util.exec("wget --quiet --output-document=%s %s", path, url))
+	end
+
+	local path_ = self.path .. "/deps/list"
+	if not util.exec("test %s -ot %s 2>/dev/null", path, path_) then
+		local xpath = '//div[@class="tabbed-block" and h3="Nexus requirements"]//td[@class="table-require-name"]/a/@href'
+		local sedexpr = [[s:^ *href="\(.*\)"$:\1:g]]
+		assert(util.exec("xmllint --html --xpath %s %s 2>/dev/null | sed %s > %s", xpath, path, sedexpr, path_))
+	end
+
+	local ret = {}
+	if self.deps then
+		for _, v in ipairs(self.deps) do
+			table.insert(ret, nexus.mod(v))
+		end
+	end
+	for l in io.lines(path_) do
+		if not (self.ignoredeps or {})[l] then
+			for _, v in ipairs(ret) do
+				if v.url == l then util.warn("Duplicate dependency: %s ==> %s", self.url, l) end
+			end
+			table.insert(ret, nexus.mod(l))
+		end
+	end
+	return ret
+end
+
+return nexus
