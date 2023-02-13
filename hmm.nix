c63dbf4ac13f09e1351b4867cc786132854f5501
Hannah Lappe <hannah@lappe.page>
2023-02-13 14:40:35 +0100

    updated nix flake

--- hmm.nix
+++ hmm.nix
@@ -1,5 +1,4 @@
-{ lib, stdenv, lua5_4, makeWrapper, p7zip, libxml2, wget, curl, rsync, which }:
-
+{ lib, stdenv, lua5_4, makeWrapper, p7zip, libxml2, wget, curl, rsync, unrar }:
 stdenv.mkDerivation rec {
  
   pname = "hmm";
@@ -9,19 +8,19 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [
     makeWrapper
-    p7zip
+  ];
+
+  buildInputs = [
+    lua5_4
     libxml2
     wget
     curl
     rsync
+    p7zip
+    unrar
   ];
 
-  buildInputs = [ lua5_4 ];
-
-
   isExecutable = true;
-
-  # dontBuild = true;
 
   buildPhase = ''
     sed -i '1 s/lua5.4/lua/' hmm.lua
@@ -29,15 +28,16 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p "$out/share/lua/hmm"
-    cp -r host/ json_lua/ util.lua json.lua "$out/share/lua/hmm" 
+    cp -r modules/ json_lua/ util.lua base.lua json.lua "$out/share/lua/hmm"
 
     mkdir "$out/bin"
-    cp hmm.lua "$out/bin/hmm.lua"
+    cp hmm.lua "$out/bin/hmm"
   '';
 
   postFixup = ''
-    wrapProgram $out/bin/hmm.lua \
-      --set LUA_PATH ";;$out/share/lua/?.lua;$out/share/lua/?/init.lua"
+    wrapProgram $out/bin/hmm \
+      --set LUA_PATH ";;$out/share/lua/?.lua;$out/share/lua/?/init.lua" \
+      --prefix PATH : "${p7zip}/bin:${libxml2}/bin:${wget}/bin:${rsync}/bin:${curl}/bin:${unrar}/bin"
   '';
 
 }
